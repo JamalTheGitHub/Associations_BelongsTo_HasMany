68d933848c7249e3a873c9d533d0109adee85783
User <ezzy_nauzzee@hotmail.com>
2019-01-10 08:04:50 +0800

    Added show action to HomeController as well as created relevant view file and modifying the routes.rb to allow connection to webpage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   get 'home/index'
+  get 'home/show'
+  
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
